76f9e8381ead968b1df07114cb97a5bc7943d6f1
Tale Prestmo <tale.prestmo@gmail.com>
2020-03-08 13:01:39 +0100

    Add undefined title check

--- src/Components/Widget.jsx
+++ src/Components/Widget.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Widget = (props) => (
   <div className="widget">
-    <h2>{props.title}</h2>
+    <h2>{props.title ? props.title : 'Tittel ikke definert'}</h2>
     {props.children}
   </div>
 );

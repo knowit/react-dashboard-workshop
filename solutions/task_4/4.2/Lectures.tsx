20116908b41f653180032db8421c00f7f552c1e6
Tale Prestmo <tale.prestmo@gmail.com>
2020-03-08 13:30:13 +0100

    Update solution on 4.2

--- solutions/task_4/4.2/Lectures.tsx
+++ solutions/task_4/4.2/Lectures.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import Widget from '../Components/Widget';
 const Lectures = () => {
   const lectures: string[] = ["Mandag 08:00-09:00 It Grunnkurs", "Onsdag 08:00-09:00 DataGK", "Torsdag 10:00-11:00 WebTek"];
   return (
-    <div className="widget">
-      <h2>Mine forelesninger</h2>
+    <Widget
+      title='Mine forelesninger'
+    >
       <ul>
-        {lectures.map((lecture: string) => <li>{lecture}</li>)}
+        {lectures.map((lecture: string) => <li key={lecture}>{lecture}</li>)}
       </ul>
-    </div>
+    </Widget>
   )
 };
 

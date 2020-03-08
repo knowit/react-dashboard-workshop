99455d1ee34d611fc052eab5213ee0e784b628ea
maynkj <maynkj@protonmail.com>
2020-03-08 17:59:56 +0100

    Update solutions for task 3.

--- solutions/task_3/3.1/Lectures.jsx
+++ solutions/task_3/3.1/Lectures.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import Widget from '../Components/Widget';
 
-const Lectures = () => (
-  <Widget>
-    <ul>
-      {
-        lectures.map(lecture =>
-          <li key={lecture}>{lecture}</li>
-        )
-      }
-    </ul>
-  </Widget>
-);
+const Lectures = () => {
+  const lectures = ["Mandag 08:00-09:00 It Grunnkurs", "Onsdag 08:00-09:00 DataGK", "Torsdag 10:00-11:00 WebTek"];
+
+  return (
+    <Widget>
+      <ul>
+        {
+          lectures.map(lecture =>
+            <li key={lecture}>{lecture}</li>
+          )
+        }
+      </ul>
+    </Widget>
+  );
+};
 
 export default Lectures;
--- solutions/task_3/3.2/Lectures.jsx
+++ solutions/task_3/3.2/Lectures.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import Widget from '../Components/Widget';
 
-const Lectures = () => (
-  <Widget
-    title='Mine forelesninger'
-  >
-    <ul>
-      {
-        lectures.map(lecture =>
-          <li key={lecture}>{lecture}</li>
-        )
-      }
-    </ul>
-  </Widget>
-);
+const Lectures = () => {
+  const lectures = ["Mandag 08:00-09:00 It Grunnkurs", "Onsdag 08:00-09:00 DataGK", "Torsdag 10:00-11:00 WebTek"];
+
+  return (
+    <Widget
+      title='Mine forelesninger'
+    >
+      <ul>
+        {
+          lectures.map(lecture =>
+            <li key={lecture}>{lecture}</li>
+          )
+        }
+      </ul>
+    </Widget>
+  );
+};
 
 export default Lectures;

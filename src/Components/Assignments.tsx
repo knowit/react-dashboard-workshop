fcd4081def2b08c86fd5d219993bc7d43ca4ec04
Tale Prestmo <tale.prestmo@gmail.com>
2020-03-08 14:05:21 +0100

    Add more help-texts

--- src/Components/Assignments.tsx
+++ src/Components/Assignments.tsx
@@ -14,7 +14,7 @@ const Assignments = () => {
   const isSubjectApproved = () => {
     // Oppgave 5.5:
     // Implementer denne metoden som returnerer true eller
-    // false avhengig om faget er bestått eller ikke
+    // false avhengig om en har nok godkjente øvinger eller ikke
   };
 
   return (
@@ -29,14 +29,15 @@ const Assignments = () => {
         }
       </div>
       <div className="subjectStatus">
+        {/* Oppgave 5.5: Her viser man statusen på faget: */}
         {/* {
             isSubjectApproved() ? 'Du er ferdig!' : 'Du er ikke helt i mål enda...'
         } */}
       </div>
       <ul>
-        {
+        { /* Oppgave 5.3: Her vises øvingene til et fag, men nå vises det kun for det første faget i lista.. */
           subjects[0].assignments.map((assignment: any, index: number) => (
-            <li key={index}>
+            <li key={index}> {/* Oppgave 5.4: Her kan man si noe om et fag er godkjent eller ikke. */}
               {assignment.name}
             </li>
           ))
